fb82ddafd2abedd9874f423fc4a3980d9ab43a2e
Sujan Lam <sujann42@gmail.com>
2025-03-01 21:56:27 -0600

    updated Product

--- src/main/java/com/fake_strore_api/model/Product.java
+++ src/main/java/com/fake_strore_api/model/Product.java
@@ -1,28 +1,22 @@
 package com.fake_strore_api.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
 @Entity
 @Table
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @JsonProperty("title")
     private String title;
-    @JsonProperty("price")
     private Float price;
-    @JsonProperty("description")
     private String description;
-    @JsonProperty("category")
     private String category;
-    @JsonProperty("image")
     private String image;
+
 }
